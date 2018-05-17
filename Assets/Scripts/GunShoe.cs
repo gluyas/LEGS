13630eb5807122c182ab7c73c4095a04d7899112
Marc <marc.gluyas@gmail.com>
2018-05-17 16:39:39 +1200

    Add base gun shoe functionality

---
+++ Assets/Scripts/GunShoe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class GunShoe : Shoe
+{
+	public float KnockbackForceMax;
+	public float KnockbackForceMin;
+
+	public float ChargeTimeMax;
+	public float ChargeTimeMin;
+	public float ChargeThreshold;
+	public float ChargeExponent;
+	[NonSerialized] public float Charge;
+
+	public Gradient ChargeColor;
+	
+	public float ProjectileSpeedMax;
+	public float ProjectileSpeedMin;
+
+	[NonSerialized] public bool IsProjectile = false;
+	
+	private void OnValidate()
+	{
+		Type = ShoeType.Gun;
+	}
+}
