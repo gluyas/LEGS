6fe85192f7114c1f00887fc024a83a555431acf5
Guest User <Guest@wg401-h-01-07.mddn.vuw.ac.nz>
2018-05-18 11:42:11 +1200

    New branch from gunboots
    merge with my menu stuff so I can use the new shoe system

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class CharController : MonoBehaviour {
+
+	private static int elementLength = 5;
+
+	// player config instance that this script sets the values of
+
+
+	// Player's selection for the stage
+
+
+	private bool isButtonPressed;
+
+	public PlayerInfo CurrentPlayerInfo;
+
+	public void Init(PlayerInfo playerInfo) {
+		this.CurrentPlayerInfo = playerInfo;
+		Debug.Log ("akjshf");
+	}
+
+	void Update () {
+		//Debug.Log (CurrentPlayerInfo.LeftShoeType);
+
+		if (!isButtonPressed) {
+			if (CurrentPlayerInfo.Controller.LeftTrigger) {
+				CurrentPlayerInfo.LeftShoeType--;
+				isButtonPressed = true;
+				Debug.Log (CurrentPlayerInfo.LeftShoeType);
+			}
+			else if (CurrentPlayerInfo.Controller.RightTrigger) {
+				CurrentPlayerInfo.LeftShoeType++;
+				isButtonPressed = true;
+				Debug.Log (CurrentPlayerInfo.LeftShoeType);
+			}
+
+			CurrentPlayerInfo.LeftShoeType = (elementLength + CurrentPlayerInfo.LeftShoeType) % elementLength;
+
+		} else if (!CurrentPlayerInfo.Controller.LeftTrigger && !CurrentPlayerInfo.Controller.RightTrigger) {
+			isButtonPressed = false;
+			//Debug.Log ("Foo");
+		}
+
+		//Debug.LogFormat ("{0}, {1}", CurrentPlayerInfo.Controller.LeftTrigger.Value, CurrentPlayerInfo.Controller.RightTrigger.Value);
+	}
+}
+
+public class PlayerInfo {
+	public int PlayerNum;
+	public InputDevice Controller;
+	public int LeftShoeType, RightShoeType;
+	public Color TeamColor;
+}
+
---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class GameplayManager : MonoBehaviour {
+
+	public GameObject CharacterControllerPrefab;
+
+	public GameObject[] CharacterSelectionUISlots;
+
+	public List<PlayerInfo> Players = new List<PlayerInfo> ();
+
+	public int amountShoes;
+	private string selectorText;
+	public int stageSelector;
+
+
+	// Use this for initialization
+	void Start () {
+		amountShoes = 2;
+
+		int playerCount = 0;
+		foreach (var device in InputManager.Devices) {
+			var playerInfo = new PlayerInfo();
+			playerInfo.Controller = device;
+			playerInfo.PlayerNum = playerCount++;
+
+			Players.Add(playerInfo);
+		}
+
+		foreach (var playerInfo in Players) {
+			var charController = Instantiate (CharacterControllerPrefab);
+			charController.GetComponent<CharController>().Init(playerInfo);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
