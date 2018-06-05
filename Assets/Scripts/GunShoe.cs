e3c7733659882c5c9397f29351f0413d38fd05f8
Marc <marc.gluyas@gmail.com>
2018-06-05 16:18:42 +1200

    Fix GunShoe damage calculation
    Add visual damage indication with path tracing gizmo
    Tweak balance

--- Assets/Scripts/GunShoe.cs
+++ Assets/Scripts/GunShoe.cs
@@ -23,35 +23,53 @@ public class GunShoe : Shoe
 	public bool IsProjectile
 	{
 		get { return Attacker != null; }
+		set {
+			if (!value)
+			{
+				Charge = 0;
+				GetComponent<Renderer>().material.color = ChargeColor.Evaluate(0);
+			
+				Attacker.IgnoreCollisions(GetComponent<Collider2D>(), false);
+				this.gameObject.layer = LayerMask.NameToLayer("Pickups");
+				Attacker = null;
+			} else Debug.Assert(false);
+		}
 	}
 	
 	public Gradient ChargeColor;
+
+	private void FixedUpdate()
+	{
+		if (!IsProjectile) return;
+		
+		var velocity = GetComponent<Rigidbody2D>().velocity;
+		if (velocity.magnitude >= ProjectileSpeedThreshold)
+		{
+			Charge = (velocity.magnitude - ProjectileSpeedMin) / (ProjectileSpeedMax - ProjectileSpeedMin);
+#if true
+			{	// path tracing
+				Color color;
+				if      (Charge >= 1) color = Color.black;
+				else if (Charge <= 0) color = Color.white;
+				else                  color = Color.Lerp(Color.blue, Color.red, Charge);
+
+				Debug.DrawRay(this.transform.position, velocity * Time.deltaTime, color, 1.5f);
+			}
+#endif
+			Charge = Mathf.Clamp01(Charge);
+		}
+		else IsProjectile = false;
+	}
 
 	private void OnCollisionEnter2D(Collision2D other)
 	{
 		if (!IsProjectile) return;
 		
-		var speed = GetComponent<Rigidbody2D>().velocity.magnitude;
-		var isProjectile = speed >= ProjectileSpeedThreshold;
-
-		if (isProjectile)
+		var target = other.gameObject.GetComponent<Player>();
+		if (target != null)
 		{
-			var target = other.gameObject.GetComponent<Player>();
-			if (target != null)
-			{
-				var damage = Mathf.Lerp(DamageMin, DamageMax, Charge);
-				Debug.LogFormat("{0} @ {1}", damage, speed);
-				if (Player.DealDamage(Attacker, target, damage)) isProjectile = false;
-			}
-		}
-		if (!isProjectile)
-		{
-			Charge = 0;
-			GetComponent<Renderer>().material.color = ChargeColor.Evaluate(0);
-			
-			Attacker.IgnoreCollisions(GetComponent<Collider2D>(), false);
-			this.gameObject.layer = LayerMask.NameToLayer("Pickups");
-			Attacker = null;
+			var damage = Mathf.Lerp(DamageMin, DamageMax, Charge);
+			if (Player.DealDamage(Attacker, target, damage)) IsProjectile = false;
 		}
 	}
 
