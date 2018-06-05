530eaa8f751ad763e54559c2c35bd340605fdc6f
Marc <marc.gluyas@gmail.com>
2018-06-05 23:22:49 +1200

    Rename HpBar into PlayerHud
    Remove HP bar from Player prefab

--- Assets/Scripts/PlayerHud.cs
+++ Assets/Scripts/PlayerHud.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HpBar : MonoBehaviour
+public class PlayerHud : MonoBehaviour
 {
 	public Player Player;
 	
